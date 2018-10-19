c829baef7fa8c7cd2f1b9bfe901437886d5cef56
vauxhall <zero.griffin@gmail.com>
2018-10-19 12:40:47 +0200

    Set app for portrait only

--- Game/Shared/Navigation.swift
+++ Game/Shared/Navigation.swift
@@ -1,8 +1,6 @@
 import UIKit
 
 class Navigation:UINavigationController {
-    override var prefersStatusBarHidden:Bool { return true }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBarHidden(true, animated:false)
