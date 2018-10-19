35b28f8a3e27e80cc112757ad715cf95968cb16b
vauxhall <zero.griffin@gmail.com>
2018-10-19 15:57:29 +0200

    Add intro

--- Game/Intro/IntroView.swift
+++ Game/Intro/IntroView.swift
@@ -1,12 +1,35 @@
 import CleanArchitecture
+import AVFoundation
 
 class IntroView:View<IntroPresenter> {
+    private weak var layer:AVPlayerLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if #available(iOS 10.0, *) {
+            try! AVAudioSession.sharedInstance().setCategory(.playback, mode:.moviePlayback)
+        }
+
         view.backgroundColor = .black
         makeOutlets()
+        
+        NotificationCenter.default.addObserver(self, selector:#selector(introEnded), name:Notification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewDidAppear(animated)
+        layer.player?.play()
     }
     
     private func makeOutlets() {
+        let player = AVPlayer(url:Bundle.main.url(forResource:"intro", withExtension:"mp4")!)
+        let layer = AVPlayerLayer(player:player)
+        layer.frame = UIScreen.main.bounds
+        view.layer.addSublayer(layer)
+        self.layer = layer
+    }
+    
+    @objc private func introEnded() {
+        Application.navigation.setViewControllers([HomeView()], animated:true)
     }
 }
