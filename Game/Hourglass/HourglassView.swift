e2fa02907b7506b114a9a870970f2428cd88111d
vauxhall <zero.griffin@gmail.com>
2018-10-24 23:30:58 +0100

    Add sk view and scene

--- Game/Hourglass/HourglassView.swift
+++ Game/Hourglass/HourglassView.swift
@@ -1,5 +1,22 @@
 import CleanArchitecture
+import SpriteKit
 
-class HourglassView<HourglassPresenter> {
+class HourglassView:View<HourglassPresenter> {
+    private weak var skView:SKView!
+    private weak var scene:SKScene!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeOutlets()
+    }
+    
+    private func makeOutlets() {
+        let skView = SKView()
+        view.addSubview(skView)
+        self.skView = skView
+        
+        let scene = SKScene(size:UIScreen.main.bounds.size)
+        skView.presentScene(scene)
+        self.scene = scene
+    }
 }
